87e6945e3eea2e47515b7a3c0af8dbefb4c1564c
erik.masny <erik.masny@gmail.com>
2021-03-10 09:38:21 +0100

    Added plotly graph to DistanceMeter, taking in real distance values

--- src/components/DistanceMeter.vue
+++ src/components/DistanceMeter.vue
@@ -1,15 +1,40 @@
 <template>
     <div>
         Distance: <span class="distance">{{ distance }}</span>km
+        <div id="tester" style="width:600px;height:250px;"></div>
     </div>
 </template>
 <script>
+import Plotly from 'plotly.js-dist';
 
 export default {
     name: 'DistanceMeter',
-    props: ['distance']
-//   components: {
-//  
+    props: ['distance'],
+    watch: {
+        distance: function(newVal, oldVal) {
+            console.log(`New: ${newVal}, old: ${oldVal}`);
+            this.graph.x.push(this.x)
+            this.graph.y.push(newVal)
+            this.x++;
+            // alert(this.graph.y)
+            Plotly.redraw(this.TESTER)
+        }
+    },
+    data() {
+        return {
+            graph: {
+                x: [0],
+                y: [0]
+            },
+            TESTER: null,
+            x: 1
+        }
+    },
+    mounted() {
+        this.TESTER = document.getElementById('tester')
+        Plotly.newPlot( this.TESTER, [this.graph], {
+        margin: { t: 0 } } );
+    }
 }
 </script>
 
